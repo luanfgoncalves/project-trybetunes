d97c84fb5365a3396a308b325883177bf24e1a29
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-17 18:17:41 -0300

    Feat: Requisito 2, Header.jsx funcional e importado em todas as páginas.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      isLoginHappening: false,
+      isLoginFinished: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  getUserData = async () => {
+    this.setState({ isLoginHappening: true });
+    const { name } = await getUser();
+    this.setState({
+      username: name,
+      isLoginHappening: false,
+      isLoginFinished: true,
+    });
+  }
+
   render() {
+    const { username, isLoginHappening, isLoginFinished } = this.state;
     return (
       <header data-testid="header-component">
         <a href="/">Login</a>
         <a href="/search">Search</a>
         <a href="/favorites">Favorites</a>
         <a href="/album/:id">Albums</a>
-        <a href="/profile">Profile</a>
+        <a href="/profile" data-testid="header-user-name">
+          { isLoginHappening && <Loading /> }
+          { isLoginFinished && username }
+        </a>
+        {/* {isLoginHappening && <Loading /> } */}
+        {/* {isLoginFinished && <a href="/profile" data-testid="header-user-name"> { username } </a> } */}
+
       </header>
     );
   }
